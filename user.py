388c28900de552433f1469e9759b4b75d0019512
Josimar Acosta <josyacosta07@gmail.com>
2025-05-21 10:42:38 -0500

    Update user.py

--- user.py
+++ user.py
@@ -1,20 +1,22 @@
 from pyrogram import Client
 from config import Config
 
-REPLY_MESSAGE=Config.REPLY_MESSAGE
+REPLY_MESSAGE = Config.REPLY_MESSAGE
 
 if REPLY_MESSAGE is not None:
     USER = Client(
-        "user_session",  # A short, valid session name
-        session_string=Config.SESSION,  # The session string goes here
+        "user_session",  # Nombre de sesión corto y válido
+        session_string=Config.SESSION,  # Cadena de sesión
         api_id=Config.API_ID,
-        api_hash=Config.API_HASH
+        api_hash=Config.API_HASH,
         plugins=dict(root="plugins.userbot")
-        )
+    )
 else:
     USER = Client(
-        Config.SESSION,
-        Config.API_ID,
-        Config.API_HASH
-        )
+        "user_session",  # Nombre de sesión corto y válido
+        session_string=Config.SESSION,  # Cadena de sesión
+        api_id=Config.API_ID,
+        api_hash=Config.API_HASH
+    )
+
 USER.start()

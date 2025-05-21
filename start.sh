1806fca5cd453e87b643922dcc3752a9ef932ba6
Josimar Acosta <josyacosta07@gmail.com>
2025-05-21 09:06:15 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
+#!/bin/bash
 echo "Cloning Repo, Please Wait..."
 git clone https://github.com/AsmSafone/RadioPlayerV3.git /RadioPlayerV3
-echo "Installing Requirements..."
 cd /RadioPlayerV3
-pip3 install -U -r requirements.txt
+
 echo "Starting Bot, Please Wait..."
-python3 main.py
+/venv/bin/python main.py
